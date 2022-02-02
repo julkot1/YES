9a20546778f93e49153ee7236d780c77745bbfc4
julkot1 <julian.kotlowski@gmail.com>
2022-02-02 23:37:29 +0100

    YELL statement

--- yes/src/main/java/pl/julkot1/yes/statement/conditional/YellStatement.java
+++ yes/src/main/java/pl/julkot1/yes/statement/conditional/YellStatement.java
@@ -1,11 +1,15 @@
 package pl.julkot1.yes.statement.conditional;
 
 import pl.julkot1.yes.ast.models.AstStatement;
+import pl.julkot1.yes.exception.InvalidArgumentsQuantity;
 import pl.julkot1.yes.exception.InvalidYesSyntaxException;
+import pl.julkot1.yes.generator.DefaultGenerators;
 import pl.julkot1.yes.statement.Statement;
+import pl.julkot1.yes.types.Type;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class YellStatement extends Statement {
     public YellStatement(AstStatement astStatement) {
@@ -14,16 +18,22 @@ public class YellStatement extends Statement {
 
     @Override
     protected void validArguments() throws InvalidYesSyntaxException {
-
+        if (!(astStatement.getArguments().size() == 2))throw new InvalidArgumentsQuantity(astStatement.getLine(), astStatement.getToken());
+        astStatement.getArgument(0).setType(Type.BOOL);
     }
 
     @Override
-    protected void write(FileOutputStream out) throws IOException {
+    protected void write(FileOutputStream out) throws IOException, InvalidYesSyntaxException {
+        out.write("if(!*((unsigned char*)cr[0])){".getBytes());
+        DefaultGenerators.writeArguments(List.of(this.astStatement.getArgument(1)), out);
+        var type = astStatement.getArgument(1).getType();
+        out.write((String.format("printf(\"%s: by YELL statement: line %d\\n\", *((%s *) cr[1]));", type.getCFormatSpecifier(), astStatement.getLine(),type.getCToken())).getBytes());
+        out.write("exit(EXIT_FAILURE);}".getBytes());
 
     }
 
     @Override
     protected void writeArguments(FileOutputStream out) throws IOException, InvalidYesSyntaxException {
-
+        DefaultGenerators.writeArguments(List.of(this.astStatement.getArgument(0)), out);
     }
 }
